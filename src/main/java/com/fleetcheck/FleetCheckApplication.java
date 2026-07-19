1f5692944868d45087bf75ee632edc07c9801a55
SethTheMountain <smsorrell27@gmail.com>
2026-07-19 02:08:14 -0400

    Phase 0: project scaffold

---
+++ src/main/java/com/fleetcheck/FleetCheckApplication.java
@@ -0,0 +1,11 @@
+package com.fleetcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetCheckApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FleetCheckApplication.class, args);
+    }
+}
